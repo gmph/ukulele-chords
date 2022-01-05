60c38bf6a030964e7c9e3dbdb2db403a731a9b7e
Graham Macphee <hi@grahammacphee.co.uk>
2022-01-05 15:13:22 +0000

    Fix server routing

--- server-js/server.js
+++ server-js/server.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var serverless = require('serverless-http');
 var expressApp = express();
-var router = express.Router();
 expressApp.use(express.static("client"));
-expressApp.get("/:chord", function (req, res) {
+var router = express.Router();
+router.get("/:chord", function (req, res) {
     var chord = req.params.chord;
     if (!chord || chord.length <= 0) {
         res.sendStatus(500);
     }
     res.status(200).send("https://ukutabs.com/chords/standard/".concat(chord, ".svg"));
     return;
-});
-expressApp.listen(process.env.PORT, function () {
-    console.log("Static server is listening on port ".concat(process.env.PORT));
 });
 expressApp.use('/.netlify/functions/server', router);
 module.exports = expressApp;
--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,17 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const expressApp = express();
-const router = express.Router();
 
+const expressApp = express();
 expressApp.use(express.static("client"));
 
-expressApp.get("/:chord", (req, res) => {
+const router = express.Router();
+router.get("/:chord", (req, res) => {
   const chord: string = req.params.chord;
   if (!chord || chord.length <= 0) {
     res.sendStatus(500);
   }
   res.status(200).send(`https://ukutabs.com/chords/standard/${chord}.svg`);
   return;
-});
-
-expressApp.listen(process.env.PORT, () => {
-  console.log(`Static server is listening on port ${process.env.PORT}`);
 });
 
 expressApp.use('/.netlify/functions/server', router);
